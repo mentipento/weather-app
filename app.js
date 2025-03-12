577c54cfb2dccde5de769296ca5394107e3695dc
mentipento <26530260+mentipento@users.noreply.github.com>
2025-03-12 11:46:58 +0100

    finalize app

--- app.js
+++ app.js
@@ -1,104 +1,151 @@
-/** *** */ (() => { // webpackBootstrap
-/** *** */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 
-  // ./src/modules/api.js
-  // modules/api.js
+;// ./src/modules/api.js
+// modules/api.js
 
-  const BASE_URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
-  const API_KEY = 'T74DFCSK6R9XDJA8TRFRLL2QH';
-  async function fetchWeatherData(location) {
-    console.log(`${BASE_URL}${location}?key=${API_KEY}`);
-    try {
-      const response = await fetch(`${BASE_URL}${location}?key=${API_KEY}`, {
-        mode: 'cors',
-      });
-      if (!response.ok) {
-        throw new Error(`API error: ${response.statusText}`);
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-      return null;
+const BASE_URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+const API_KEY = 'T74DFCSK6R9XDJA8TRFRLL2QH';
+async function fetchWeatherData(location) {
+  console.log(`${BASE_URL}${location}?key=${API_KEY}`);
+  try {
+    const response = await fetch(`${BASE_URL}${location}?key=${API_KEY}`, {
+      mode: 'cors'
+    });
+    if (!response.ok) {
+      throw new Error(`API error: ${response.statusText}`);
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
-  // ./src/modules/helpers.js
-  // modules/helpers.js
+}
+;// ./src/modules/helpers.js
+// modules/helpers.js
 
-  function fahrenheitToCelsius(degree) {
-    return (degree - 32) * 5 / 9;
-  }
-  function celsiusToFahrenheit(degree) {
-    return degree * 9 / 5 + 32;
-  }
-  // ./src/modules/weather.js
-  // modules/weather.js
+function fahrenheitToCelsius(degree) {
+  return (degree - 32) * 5 / 9;
+}
+function celsiusToFahrenheit(degree) {
+  return degree * 9 / 5 + 32;
+}
+;// ./src/modules/weather.js
+// modules/weather.js
 
-  let useCelsius = true;
-  function setTemperatureUnit(isCelsius) {
-    useCelsius = isCelsius;
+
+let useCelsius = true;
+function setTemperatureUnit(isCelsius) {
+  useCelsius = isCelsius;
+}
+function processWeatherData(data) {
+  return {
+    address: data.resolvedAddress,
+    temperature: useCelsius ? Math.round(fahrenheitToCelsius(data.currentConditions.temp)) : Math.round(data.currentConditions.temp),
+    conditions: data.currentConditions.conditions,
+    icon: data.currentConditions.icon
+  };
+}
+;// ./src/modules/dom.js
+// modules/dom.js
+
+function updateWeatherUI(weatherData) {
+  const locationElement = document.querySelector('.display-location');
+  const temperatureElement = document.querySelector('.display-temperature');
+  const iconElement = document.getElementById('placeholder');
+  const conditionsElement = document.querySelector('.display-conditions');
+  if (!weatherData) {
+    locationElement.textContent = '⚠️ Location not found';
+    temperatureElement.textContent = '';
+    conditionsElement.textContent = '';
+    iconElement.src = '';
+    return;
   }
-  function processWeatherData(data) {
-    return {
-      address: data.resolvedAddress,
-      temperature: useCelsius ? Math.round(fahrenheitToCelsius(data.currentConditions.temp)) : Math.round(data.currentConditions.temp),
-      conditions: data.currentConditions.conditions,
-      icon: data.currentConditions.icon,
+  locationElement.textContent = weatherData.address;
+  temperatureElement.textContent = `${weatherData.temperature}`;
+  iconElement.style.visibility = 'visible';
+  iconElement.src = `./assets/images/WeatherIcons-main/SVG/2nd_Set_-_Color/${weatherData.icon}.svg`;
+  conditionsElement.textContent = weatherData.conditions;
+}
+;// ./src/modules/geolocation.js
+// modules/geolocation.js
+
+function fetchGeoLocation() {
+  return new Promise((resolve, reject) => {
+    const options = {
+      maximumAge: 0,
+      enableHighAccuracy: false
     };
-  }
-  // ./src/modules/dom.js
-  // modules/dom.js
+    const success = pos => {
+      const {
+        coords
+      } = pos; // same as const coords = pos.coords
+      console.log(coords);
+      resolve(coords);
+    };
+    const error = err => {
+      console.error("❌ Geolocation Error:", err);
+      reject(err);
+    };
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  });
+}
+;// ./src/index.js
+// index.js
 
-  function updateWeatherUI(weatherData) {
-    const locationElement = document.querySelector('.display-location');
-    const temperatureElement = document.querySelector('.display-temperature');
-    const iconElement = document.getElementById('placeholder');
-    const conditionsElement = document.querySelector('.display-conditions');
-    if (!weatherData) {
-      locationElement.textContent = '⚠️ Location not found';
-      temperatureElement.textContent = '';
-      conditionsElement.textContent = '';
-      iconElement.src = '';
-      return;
-    }
-    locationElement.textContent = weatherData.address;
-    temperatureElement.textContent = `${weatherData.temperature}`;
-    iconElement.style.visibility = 'visible';
-    iconElement.src = `./assets/images/WeatherIcons-main/SVG/2nd_Set_-_Color/${weatherData.icon}.svg`;
-    conditionsElement.textContent = weatherData.conditions;
-  }
-  // ./src/index.js
-  // index.js
 
-  async function updateWeatherForCurrentLocation() {
-    const location = document.getElementById('input-location').value;
-    if (!location) {
-      console.error('No location entered.');
-      return;
-    }
+
+
+
+
+async function updateWeatherForCurrentLocation() {
+  const location = document.getElementById('input-location').value;
+  if (!location) {
+    console.error('No location entered.');
+    return;
+  }
+  const rawData = await fetchWeatherData(location);
+  if (!rawData) {
+    console.error('Could not fetch weather data');
+    updateWeatherUI(null);
+    return;
+  }
+  const weatherData = processWeatherData(rawData);
+  updateWeatherUI(weatherData);
+}
+document.getElementById('form-location').addEventListener('submit', async event => {
+  event.preventDefault();
+  updateWeatherForCurrentLocation();
+});
+document.getElementById('checkbox-celsius').addEventListener('change', event => {
+  if (event.target.checked) {
+    document.querySelector('.celsius-placeholder').textContent = 'F°';
+    setTemperatureUnit(false);
+  } else {
+    document.querySelector('.celsius-placeholder').textContent = 'C°';
+    setTemperatureUnit(true);
+  }
+  updateWeatherForCurrentLocation();
+});
+document.getElementById('locate-icon').addEventListener('click', async () => {
+  try {
+    const coords = await fetchGeoLocation();
+    console.log(`🌍 Location: ${coords.latitude},${coords.longitude}`);
+    const location = `${coords.latitude.toFixed(4)},${coords.longitude.toFixed(4)}`;
+    document.getElementById('input-location').value = location;
     const rawData = await fetchWeatherData(location);
     if (!rawData) {
-      console.error('Could not fetch weather data');
+      console.error("❌ Couldn't fetch weather data");
       updateWeatherUI(null);
       return;
     }
     const weatherData = processWeatherData(rawData);
     updateWeatherUI(weatherData);
+  } catch (error) {
+    console.error('⚠️ Geolocation could not be obtained');
   }
-  document.getElementById('form-location').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    updateWeatherForCurrentLocation();
-  });
-  document.getElementById('checkbox-celsius').addEventListener('change', (event) => {
-    if (event.target.checked) {
-      document.querySelector('.celsius-placeholder').textContent = 'F°';
-      setTemperatureUnit(false);
-    } else {
-      document.querySelector('.celsius-placeholder').textContent = 'C°';
-      setTemperatureUnit(true);
-    }
-    updateWeatherForCurrentLocation();
-  });
-/** *** */ })();
-
-// # sourceMappingURL=app.js.map
+});
+/******/ })()
+;
+//# sourceMappingURL=app.js.map
--- assets/images/WeatherIcons-main/README.md
+++ assets/images/WeatherIcons-main/README.md
@@ -2,18 +2,19 @@
 
 ## Sets of themed weather icons to beautify your weather app and web pages
 
-These 8 sets of weather icons (4 in monochrome and 4 in color) are designed to improve the presentation of your weather data wherever it may be.  Each icon set has 22 icons in both SVG vector format and PNG image format.  Use them on your webpage, in any app, or in your presentation.  They align with the IDs used by the Visual Crossing Weather API (https://www.visualcrossing.com/weather-api) and may also be compatible with other providers as well.
+These 8 sets of weather icons (4 in monochrome and 4 in color) are designed to improve the presentation of your weather data wherever it may be. Each icon set has 22 icons in both SVG vector format and PNG image format. Use them on your webpage, in any app, or in your presentation. They align with the IDs used by the Visual Crossing Weather API (https://www.visualcrossing.com/weather-api) and may also be compatible with other providers as well.
 
 ## Basic Usage
 
-At the top level of this repository you will find an index sheet for each icon set.  You will find the icons themselves in the SVG and PNG folders.  To use an icon or icon set, simply navigate into the appropriate format folder, find the subfolder for the icon set that you want to use, and add one or all to your project.
+At the top level of this repository you will find an index sheet for each icon set. You will find the icons themselves in the SVG and PNG folders. To use an icon or icon set, simply navigate into the appropriate format folder, find the subfolder for the icon set that you want to use, and add one or all to your project.
 
 ## Licensing
 
-These icons are free for your use under the GNU LGPL (https://www.gnu.org/licenses/lgpl-3.0.txt).  That means that you can use them for both commercial and non-commercial purposes without restriction.  If you do modify them or make improvements, we ask that you do as we have done and make your updates available under an open source license so that everyone can use them freely.  (Of course, under the LGPL, that is your decision.)
+These icons are free for your use under the GNU LGPL (https://www.gnu.org/licenses/lgpl-3.0.txt). That means that you can use them for both commercial and non-commercial purposes without restriction. If you do modify them or make improvements, we ask that you do as we have done and make your updates available under an open source license so that everyone can use them freely. (Of course, under the LGPL, that is your decision.)
 
 ## Samples
-(Note, these are just samples.  More icons are available in the folders.)
+
+(Note, these are just samples. More icons are available in the folders.)
 
 1st Set Monochrome
 
@@ -126,6 +127,3 @@ These icons are free for your use under the GNU LGPL (https://www.gnu.org/licens
 ![Icon Sample](https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/4th%20Set%20-%20Color/snow.png)
 ![Icon Sample](https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/4th%20Set%20-%20Color/thunder-rain.png)
 ![Icon Sample](https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/PNG/4th%20Set%20-%20Color/wind.png)
-
-
-
